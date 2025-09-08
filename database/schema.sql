fd88d186b838678201a27cd7f52cd09473afc360
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-09-09 00:23:29 +0900

    initial commit

---
+++ database/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE families (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    family_name VARCHAR(100) NOT NULL,
+    kids INT NOT NULL,
+    adults INT NOT NULL,
+    prefecture VARCHAR(50),
+    city_ward VARCHAR(100),
+    block_number VARCHAR(50),
+    building VARCHAR(100),
+    postal_code VARCHAR(10)
+)
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    phone VARCHAR(16) UNIQUE,
+    rakuten_id VARCHAR(100) UNIQUE,
+    family_id INT,
+    FOREIGN KEY (family_id) REFERENCES families(id)
+);
+
+CREATE TABLE items (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    owner_id INT NOT NULL,
+    item_name VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL,
+    date_expiry TIMESTAMP,
+    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+)
