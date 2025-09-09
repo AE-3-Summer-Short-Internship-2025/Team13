5f5e1a204d0d518c524a2b6296aeac8f46348a33
takaaa <nakamizu916@gmail.com>
2025-09-09 18:00:25 +0900

    Mypage.jsxに表示されるゲストアイコンの追加と全体の見た目の補正

--- frontend/src/Mypage.jsx
+++ frontend/src/Mypage.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
+import icon from './assets/icon.svg'
 
 function MyPage() {
-
-  const [timeout, setTimeout] = useState(false);
 
   const userData = {
     name: "ゲスト",
@@ -18,27 +17,30 @@ function MyPage() {
             <h1>My Page</h1>
         </header>
 
-        <h2>{userData.name}</h2>
-        <p>ユーザーID: {userData.user_id}</p>
-        <p>ファミリーID: {userData.family_id}</p>
-        <p>大人の人数: {userData.adult_num}人</p>
-        <p>子供の人数: {userData.child_num}人</p>
-
-      {/* <h1></h1>
-      <h1>あなたの備蓄</h1>
-      <h3 onClick={() => setTimeout(prev => !prev)}>期限切れ</h3>
-      <svg style={{ transform: `${timeout ? 'rotate(0deg)' : 'rotate(-90)'}` }} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#50b4aa" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,93.66-40,40a8,8,0,0,1-11.32,0l-40-40a8,8,0,0,1,11.32-11.32L128,140.69l34.34-34.35a8,8,0,0,1,11.32,11.32Z"></path></svg>
-      {timeout && fakeData.map(item => {
-        return (
-          <>
-            <div key={item.name}>
-              <p>{item.name}</p>
-              <p>{item.count}</p>
-              <p>{item.date}</p>
+        <main style = {{display: 'flex', alignItems: 'center', backgroundColor: '#D3D3D3', padding: '.5em 1em', borderRadius: '1em'}}>
+            <img src={icon} alt="icon" style={{borderRadius:'10em', backgroundColor:'rgb(210, 230, 225)'}}/>
+            <div style = {{marginLeft: '1em'}}>
+                <h2>{userData.name}</h2>
+                <p>ユーザーID: {userData.user_id}</p>
+                <p>ファミリーID: {userData.family_id}</p>
             </div>
-          </>
-        );
-      })} */}
+        </main>
+
+        <sub>
+            <div>
+                <h3>家族メンバー</h3>
+                <p>大人人数: <span style={{ fontWeight: 'bold', backgroundColor: '#D3D3D3', borderRadius: '0.5em', padding: '.3em 2em'}}>{userData.adult_num}</span>人</p>
+                <p>子供人数: <span style={{ fontWeight: 'bold', backgroundColor: '#D3D3D3', borderRadius: '0.5em', padding: '.3em 2em'}}>{userData.child_num}</span>人</p>
+            </div>
+        </sub>
+
+        <h3>各種設定</h3>
+        <div style={{display: 'flex', flexDirection: 'column', backgroundColor: '#D3D3D3', borderRadius: '0.5em'}}>
+            <a style={{borderBottom: '.1em solid grey', padding:'.5em'}}>基本情報の編集</a>
+            <a style={{borderBottom: '.1em solid grey', padding:'.5em'}}>通知</a>
+            <a style={{borderBottom: '.1em solid grey', padding:'.5em'}}>ヘルプ</a>
+            <a style={{padding:'.5em'}}>ログアウト</a>
+        </div>
     </>
   );
 }
