20db5583070a2f94a94de1ed0f01d0f65f90f259
takaaa <nakamizu916@gmail.com>
2025-09-09 15:47:48 +0900

    Mypage .jsxの作成

--- frontend/src/Mypage.jsx
+++ frontend/src/Mypage.jsx
@@ -4,23 +4,27 @@ function MyPage() {
 
   const [timeout, setTimeout] = useState(false);
 
-  const fakeData = [
-    {
-      name: "かんぱん",
-      count: 2,
-      date: "2025-7-6"
-    },
-    {
-      name: "鯖缶",
-      count: 3,
-      date: "2025-7-5"
-    }
-  ];
+  const userData = {
+    name: "ゲスト",
+    user_id: "1",
+    family_id: "1",
+    adult_num: "2",
+    child_num: "1"
+    };
 
   return (
     <>
-      <h1>My Page</h1>
-      <h1>今日の伝言板</h1>
+        <header>
+            <h1>My Page</h1>
+        </header>
+
+        <h2>{userData.name}</h2>
+        <p>ユーザーID: {userData.user_id}</p>
+        <p>ファミリーID: {userData.family_id}</p>
+        <p>大人の人数: {userData.adult_num}人</p>
+        <p>子供の人数: {userData.child_num}人</p>
+
+      {/* <h1></h1>
       <h1>あなたの備蓄</h1>
       <h3 onClick={() => setTimeout(prev => !prev)}>期限切れ</h3>
       <svg style={{ transform: `${timeout ? 'rotate(0deg)' : 'rotate(-90)'}` }} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#50b4aa" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,93.66-40,40a8,8,0,0,1-11.32,0l-40-40a8,8,0,0,1,11.32-11.32L128,140.69l34.34-34.35a8,8,0,0,1,11.32,11.32Z"></path></svg>
@@ -34,7 +38,7 @@ function MyPage() {
             </div>
           </>
         );
-      })}
+      })} */}
     </>
   );
 }
